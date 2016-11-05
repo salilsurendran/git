b0c8cf2c30293b498590bf594d1568c10e6bd60c
Salil Surendran <salilsurendran@cloudera.com>
2016-11-04 18:12:10 -0700

    //Change made in Git2 that will not appear in Git1 if a fetch is not done and and just a git merge is done

--- src/main/java/com/salil/git/GitTest3.java
+++ src/main/java/com/salil/git/GitTest3.java
@@ -8,4 +8,6 @@ package com.salil.git;
 public class GitTest3 {
     //A change made in a different working folder and pushed to upstream repo
     //Change made in Git1 that shouldn't appear in Git2 folder with just a git fetch
+    //Change made in Git2 that will not appear in Git1 if a fetch is not done and and just a
+    //git merge is done
 }
